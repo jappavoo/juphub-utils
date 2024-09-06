00fdc85279a48bd869e5f217106a65173ab4b661
Jonathan Appavoo <jappavoo@bu.edu>
2024-09-06 07:05:46 +0000

    more advanced version reenabling jupyter interface

--- reenable_jup.sh
+++ reenable_jup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -x
 
 JUPY_EXTS="@jupyterlab/fileeditor-extension:plugin
 @jupyterlab/filebrowser-extension:widget
@@ -22,21 +23,36 @@ jupyterlab-jupytext
 @jupyterlab/workspaces-extension
 @jupyterlab/git"
 
+#JUPY_EXTS=""
+
 for ext in ${JUPY_EXTS}; do
     echo "Enabling: $ext"
     jupyter labextension enable $ext
 done
 
-echo "resetting /opt/conda/share/jupyter/lab/settings/overrides.json"
-cat <<EOF > /opt/conda/share/jupyter/lab/settings/overrides.json
-{
-    "@jupyterlab/terminal-extension:plugin": {
-        "shutdownOnClose": true
-    },
-    "@jupyterlab/statusbar-extension:plugin": {
-        "visible": false
-    }
-}
-EOF
+# Menu settings seem to be defined here
+# /opt/conda/share/jupyter/lab/schemas/@jupyterlab/mainmenu-extension/plugin.json
+# we assume that some one may have disabled some of the menus in the overrides
+# /opt/conda/share/jupyter/lab/settings/overrides.json
+
+# we are making the arbitrary choice to create a user prefrences that enables all
+# menu items that may have been disable in the overrides
+OVER_RIDES="/opt/conda/share/jupyter/lab/settings/overrides.json"
+USER_MENU_SETTINGS="${HOME}/.jupyter/lab/user-settings/@jupyterlab/mainmenu-extension/plugin.jupyterlab-settings"
+
+if [[ -a ${OVER_RIDES} ]]; then
+   echo "Found ${OVER_RIDES}"
+   if [[ -a ${USER_MENU_SETTINGS} ]]; then
+       TS=$(date +'%F-%s')
+       echo "  backing up ${USER_MENU_SETTINGS} ${USER_MENU_SETTINGS}.${TS}"
+       cp ${USER_MENU_SETTINGS} ${USER_MENU_SETTINGS}.${TS}
+   fi
+   echo "creating ${USER_MENU_SETTINGS} by rewriting ${OVER_RIDES}"
+   python -c "import json
+f = open('${OVER_RIDES}')
+resp = json.load(f)
+print(json.dumps(resp['@jupyterlab/mainmenu-extension:plugin'], indent=4))
+" | sed -e 's/"disabled": true,/"disabled": false,/g'  > ${USER_MENU_SETTINGS}
+fi
 
 echo "reload jupyterlab webpage for new setting to take effect"
